993deca1a25f084bf52b6c41bd77d41d76bd38ee
Fredrik <fredrik.jonson@student.kyh.se>
2022-11-02 15:06:34 +0100

    Swagger

--- ElevatorApi/Program.cs
+++ ElevatorApi/Program.cs
@@ -13,8 +13,11 @@ var app = builder.Build();
 
 app.UseSwagger();
 
-app.UseSwaggerUI();
-
+app.UseSwaggerUI(options =>
+{
+    options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+    options.RoutePrefix = string.Empty;
+});
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
