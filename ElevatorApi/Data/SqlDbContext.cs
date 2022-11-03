11a70f92c5af51e16c0c1b2bc712732fc5f9ecbe
Fredrik <fredrik.jonson@student.kyh.se>
2022-11-03 09:44:13 +0100

    Added EFCore, DbContext, Azure Keyvault

---
+++ ElevatorApi/Data/SqlDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ElevatorApi.Data
+{
+    public class SqlDbContext : DbContext
+    {
+        public SqlDbContext(DbContextOptions<SqlDbContext> options) : base(options)
+        {
+            
+        }
+    }
+}
