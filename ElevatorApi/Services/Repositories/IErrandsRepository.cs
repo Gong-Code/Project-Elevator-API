9144ba23929d65aa8cacd4cca545e843429e2ed4
Fredrik <fredrik.jonson@student.kyh.se>
2022-11-14 13:49:13 +0100

    ElevatorId
    Added elevator id to dto in getby id

--- ElevatorApi/Services/Repositories/IErrandsRepository.cs
+++ ElevatorApi/Services/Repositories/IErrandsRepository.cs
@@ -4,13 +4,15 @@ using ElevatorApi.Helpers.Extensions;
 using ElevatorApi.Models.Elevator;
 using ElevatorApi.Models.Errands;
 using ElevatorApi.ResourceParameters;
+using IdentityModel.Client;
 using Microsoft.EntityFrameworkCore;
 
 namespace ElevatorApi.Services.Repositories;
 
 public interface IErrandsRepository
 {
-    public Task<(IEnumerable<ErrandDto> Elevators, PaginationMetadata PaginationMetadata, bool IsSuccess)> GetErrandsWithoutElevatorId(ErrandsResourceParameters parameters);
+    public Task<(IEnumerable<ErrandDto> Elevators, PaginationMetadata PaginationMetadata, bool IsSuccess)> GetErrandsWithoutElevatorIdAsync(ErrandsResourceParameters parameters);
+    public Task<(ErrandDto? Errand, bool IsSuccess)> GetErrandAsync(Guid elevatorId, Guid errandId);
 }
 
 public class ErrandsRepository : IErrandsRepository
@@ -24,7 +26,7 @@ public class ErrandsRepository : IErrandsRepository
         _mapper = mapper;
     }
 
-    public async Task<(IEnumerable<ErrandDto> Elevators, PaginationMetadata PaginationMetadata, bool IsSuccess)> GetErrandsWithoutElevatorId(ErrandsResourceParameters parameters)
+    public async Task<(IEnumerable<ErrandDto> Elevators, PaginationMetadata PaginationMetadata, bool IsSuccess)> GetErrandsWithoutElevatorIdAsync(ErrandsResourceParameters parameters)
     {
         try
         {
@@ -67,5 +69,34 @@ public class ErrandsRepository : IErrandsRepository
         }
 
         return (Enumerable.Empty<ErrandDto>(), null, false)!;
+    }
+
+    public async Task<(ErrandDto? Errand, bool IsSuccess)> GetErrandAsync(Guid elevatorId, Guid errandId)
+    {
+        try
+        {
+            var errand = await _context.Errands.Where(x => x.ElevatorEntity.Id == elevatorId && x.Id == errandId).Select(x => new ErrandDto()
+            {
+                Title = x.Title,
+                Description = x.Description,
+                ErrandStatus = x.ErrandStatus.GetErrandStatusAsString(),
+                AssignedToName = x.AssignedToName,
+                CreatedByName = x.CreatedByName,
+                AssignedToId = x.AssignedToId,
+                CreatedById = x.CreatedById,
+                CreatedDateUtc = x.CreatedDateUtc,
+                Id = x.Id,
+                ElevatorId = x.ElevatorEntity.Id,
+            })
+                .FirstOrDefaultAsync();
+
+            return (errand, true);
+        }
+        catch
+        {
+            // ignored
+        }
+
+        return (null, false);
     }
 }
