353c03bce8381e5673fb62150f8d0e789b57456b
Fredrik <fredrik.jonson@student.kyh.se>
2022-11-22 19:45:58 +0100

    fix

--- ElevatorApi/Models/Errands/UpdateErrandDto.cs
+++ ElevatorApi/Models/Errands/UpdateErrandDto.cs
@@ -6,13 +6,13 @@ namespace ElevatorApi.Models.Errands
     {
         [Required]
         [StringLength(100, MinimumLength = 2, ErrorMessage = "The {0} value must be between {1} and {2} chars long")]
-        public string Title { get; } = null!;
+        public string Title { get; set; } = null!;
         [Required]
         [StringLength(500, MinimumLength = 2, ErrorMessage = "The {0} value must be between {1} and {2} chars long")]
-        public string Description { get; } = null!;
+        public string Description { get; set; } = null!;
         [Required]
         [RegularExpression("new|inprogress|completed", ErrorMessage = "Status must be new, inprogress, completed.")]
-        public string ErrandStatus { get; } = null!;
+        public string ErrandStatus { get; set; } = null!;
         [Required]
         public Guid AssignedToId { get; set; }
     }
