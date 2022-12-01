916cf19535b2a31eed66f433da747f1b74b0f94e
Gong-Code <gong.moonphruk@gmail.com>
2022-12-01 14:26:33 +0100

    Added statistics Get action method - with mock data

---
+++ ElevatorApi.Tests/ControllerTests/ElevatorsTest.cs
@@ -0,0 +1,20 @@
+﻿using ElevatorApi.Controllers;
+using ElevatorApi.Data;
+using ElevatorApi.Models.Elevator;
+using ElevatorApi.ResourceParameters;
+
+namespace ElevatorApi.Tests.ControllerTests
+{
+    public class ElevatorsTest
+    {
+        private readonly SqlDbContext _context;
+
+        private readonly ElevatorsController _sut;
+
+        public ElevatorsTest()
+        {
+            var elevators = new Mock<ElevatorResourceParameters>();
+            
+        }
+    }
+}
---
+++ ElevatorApi/Controllers/StatisticController.cs
@@ -0,0 +1,37 @@
+﻿using ElevatorApi.Data.Entities;
+using ElevatorApi.Models.StatisticDtos;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ElevatorApi.Controllers
+{
+    [Route("api/statistic")]
+    [ApiController]
+    public class StatisticController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetStatistics()
+        {
+            var statistics = StatisticEntity.CurrentStatistic;
+
+            if (statistics == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(statistics);
+        }
+
+        [HttpGet("{statisticId}")]
+        public ActionResult<Statistic> GetStatisticById(int statisticId)
+        {
+            var statistic = StatisticEntity.CurrentStatistic.Statistics.FirstOrDefault(f => f.Id == statisticId);
+
+            if (statistic == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(statistic);
+        }
+    }
+}
